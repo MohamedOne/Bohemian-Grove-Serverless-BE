5612ac07d33409379cb19a21224e83867357f6ea
Jared Burkamper <jaredburkamper@gmail.com>
2021-07-29 03:50:53 -0500

    Some config and lambdas

--- jest.config.ts
+++ jest.config.ts
@@ -26,7 +26,7 @@ export default {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ["Global/*"],
+  coveragePathIgnorePatterns: ["Global/*", "TestS3Fn/*"],
 
   // Indicates which provider should be used to instrument code for coverage
   // coverageProvider: "babel",
