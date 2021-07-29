38d7f98cda8eee2b4e975720a8bd9407920eb80e
Jared Burkamper <jaredburkamper@gmail.com>
2021-07-29 06:04:40 -0500

    Fixed template and created auth lambdas and tests

---
+++ src/SignUpFn/SignUp.ts
@@ -0,0 +1,31 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { HTTPResponse } from "../Global/DTO";
+import cogClient from "../Global/Cognito";
+import { SignUpCommand, SignUpCommandInput } from "@aws-sdk/client-cognito-identity-provider";
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<HTTPResponse> => {
+    const data = JSON.parse(event.body || '{}');
+
+    if (!data.userName || !data.password || !data.email) return new HTTPResponse(400, {message: "Invalid input"});
+
+    const cognitoParams: SignUpCommandInput = {
+        ClientId: process.env.COG_CLIENT_APPID,
+        Username: data.userName,
+        Password: data.password,
+        UserAttributes: [
+            {
+                Name: 'email',
+                Value: data.email
+            }
+        ]
+    }
+
+    try {
+        const result = await cogClient.send(new SignUpCommand(cognitoParams));
+    } catch (err) {
+        console.log(err);
+        return new HTTPResponse(500, {message: "Failed to create user"});
+    }
+
+    return new HTTPResponse(201);
+}
