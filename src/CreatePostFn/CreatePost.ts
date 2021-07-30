4ab56eaf93299b555a984b80c4b19d06ef33e4ab
Jared Burkamper <jaredburkamper@gmail.com>
2021-07-30 06:11:35 -0500

    Fixed it

--- src/CreatePostFn/CreatePost.ts
+++ src/CreatePostFn/CreatePost.ts
@@ -1,14 +1,14 @@
 import { HTTPResponse } from "../Global/DTO";
 import { ddbDocClient } from "../Global/DynamoDB";
 import { APIGatewayProxyEvent } from "aws-lambda"
-import {PutCommand, PutCommandInput} from "@aws-sdk/lib-dynamodb";
+import { PutCommand, PutCommandInput } from "@aws-sdk/lib-dynamodb";
 import Post from "../Global/Post";
 
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<HTTPResponse> => {
 
     //Check if we have event body and specifically if we have a username in event
-    if(event.body && event.requestContext.authorizer) {
+    if (event.body && event.requestContext.authorizer) {
         let body = event.body;
         const post: any = JSON.parse(body);
         const newPost: Post = new Post(post);
@@ -17,33 +17,33 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<HTTPResponse
         let timeStamp = temp.toString();
 
         //Check username in incoming post body against username in { event.requestContext.authorizer.claims.username }
-        
-            //Proceed with adding new post 
-            const params: PutCommandInput = {
-                TableName: process.env.DDB_Table_Name, 
 
-                Item: {
-                    dataKey: timeStamp,
-                    dataType : "post",
-                    displayName: `${newPost.displayName}`,
-                    userName: `${newPost.userName}`,
-                    displayImg: `${newPost.displayImg}`,
-                    postBody: `${newPost.postBody}`,
-                    likes: `${newPost.likes}`,
-                    comments: `${newPost.comments}`,
-                },
-                ReturnValues: "ALL_OLD"
+        //Proceed with adding new post 
+        const params: PutCommandInput = {
+            TableName: process.env.DDB_TABLE_NAME,
+
+            Item: {
+                dataKey: timeStamp,
+                dataType: "post",
+                displayName: `${newPost.displayName}`,
+                userName: `${newPost.userName}`,
+                displayImg: `${newPost.displayImg}`,
+                postBody: `${newPost.postBody}`,
+                likes: [],
+                comments: []
             }
-        
-        
-                const data = await ddbDocClient.send(new PutCommand(params));
-                return new HTTPResponse(200, "Post added successfully", data.Attributes);
-            
-        
-        
+        }
 
-    } 
-        return new HTTPResponse(400, "Unable to add post", "most outer if-else");
+        try {
+            const data = await ddbDocClient.send(new PutCommand(params));
+        } catch (err) {
+            console.log(err);
+            return new HTTPResponse(500, "Failed to add post to database");
+        }
+        
+        return new HTTPResponse(200, "Post added successfully");
 
+    }
     
+    return new HTTPResponse(400, "Unable to add post", "most outer if-else");
 }
